c3466c2edfbf668dd1fd8f4b141d8b2153d0f22b
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-08-02 15:45:06 +0000

    Fake package

---
+++ lib/Daedalus/Iris/Hydra.pm
@@ -0,0 +1,23 @@
+package Daedalus::Iris::Hydra;
+
+our $VERSION = 0.01;
+
+=head1 NAME
+
+Daedalus::Iris::Hydra - Multithread daemon which process Daedalus Project Notifications
+
+=head1 DESCRIPTION
+
+This is a stub module, see F<script/iris-hydra.pl> for details of the app.
+
+=head1 AUTHOR
+
+Álvaro Castellano Vela, C<< <alvaro.castellano.vela at gmail.com> >>
+
+=head1 LICENSE
+
+GNU GENERAL PUBLIC LICENSE Version 3
+
+=cut
+
+1;
